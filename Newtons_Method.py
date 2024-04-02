7334843cd99659d3298a7b9145d167bbed77f5e6
EduardoC <ec340121@gmail.com>
2024-04-01 21:22:26 -0500

    bisection without diff

--- Newtons_Method.py
+++ Newtons_Method.py
@@ -1,18 +1,9 @@
 def f(x): #Objective function
-    if x<0:
-        return 4*x**3 + 3*x**4
-    else:
-        return 4*x**3 - 3*x**4
+    return x**2+2*x
 def df(x):
-    if x<0:
-        return 12*x**2 + 12*x**3
-    else:
-        return 12*x**2 - 12*x**3
+    return 2*x+2
 def d2f(x):
-    if x<0:
-        return 24*x + 36*x**2
-    else:
-        return 24*x - 36*x**2
+    return 2
 
 def newton_optimization(f,df,d2f,x0,tol=1e-6,max_iter=100):
     x=[x0]
@@ -22,7 +13,7 @@ def newton_optimization(f,df,d2f,x0,tol=1e-6,max_iter=100):
             break
     return k, x[-1]
 
-k, x_min= newton_optimization(f, df, d2f,x0=-0.8)
+k, x_min= newton_optimization(f, df, d2f,x0=2)
 
 print("x =", x_min, "-->","f(x) =", f(x_min))
 print(k, "Iterations")
--- bisection.py
+++ bisection.py
@@ -1,24 +1,26 @@
 from sympy import symbols, diff
 import numpy as np 
 
-def bisection(f,a, b, l):
+def f(x):
+    return x**2+2*x
+def df(x):
+    return 2*x+2
+
+def bisection(f,df,a, b, l):
     n = np.log(l/(b-a))/np.log(0.5)
-    df=diff(f,x)
     for k in range(0, int(np.ceil(n))):
         lambda_k=0.5*(a+b)
-        if df.subs(x,lambda_k) == 0:
+        if df(lambda_k) == 0:
             return lambda_k, a, b, k
-        elif df.subs(x,lambda_k) > 0:
+        elif df(lambda_k) > 0:
             b = lambda_k
         else:
             a = lambda_k
     lambda_k=0.5*(a+b)
     return lambda_k ,a ,b, k
 
-x=symbols('x')
-f=x**2+2*x
-x_opt, a, b, k = bisection(f,-3,6,0.2)
+x_opt, a, b, k = bisection(f,df,-3,6,0.2)
 
 print("Final interval: [", a,",",b,"]")
-print("x =",x_opt, "-->","f(x) =",f.subs(x,x_opt))
+print("x =",x_opt, "-->","f(x) =",f(x_opt))
 print(k, "Iterations")
