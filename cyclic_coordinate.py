70c035686631bfddf42b1dd9927f1519004880a3
ecaballero <ec340121@gmail.com>
2024-04-02 18:57:42 -0500

    cyclic coordinate with golden section added

--- cyclic_coordinate.py
+++ cyclic_coordinate.py
@@ -1,4 +1,19 @@
 import numpy as np
+def golden_section(f, y, d_j, a, b, tol):
+    l = a + (1-0.618)*(b-a)
+    u = a + 0.618*(b-a)
+    for _ in range(100):
+        if (b-a) < tol:
+            break
+        elif f(y + l*d_j) > f(y + u*d_j):
+            a = l
+            l = u
+            u = a + 0.618*(b-a)
+        elif f(y + l*d_j) < f(y + u*d_j):
+            b = u
+            u = l
+            l = a + (1-0.618)*(b-a)
+    return a, b
 
 def cyclic_coordinate(f,x0,tol):
     n=len(x0)
@@ -16,4 +31,3 @@ def cyclic_coordinate(f,x0,tol):
 
             
         
-    
